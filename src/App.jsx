bc93303471d1042550c4d328daef48062ddbc90a
Thành Vũ <csnsjh@gmail.com>
2021-11-26 14:55:22 +0700

    feat: detect offline-online

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import { Offline, Online } from 'react-detect-offline';
 import { BrowserRouter } from 'react-router-dom';
+import OfflineScreen from './components/common/OfflineScreen';
 import DefaultLayout from './components/layouts/DefaultLayout';
 import Screens from './screens';
 import theme from './themes/defaultTheme';
@@ -12,7 +14,12 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <BrowserRouter>
           <DefaultLayout>
-            <Screens />
+            <Online>
+              <Screens />
+            </Online>
+            <Offline>
+              <OfflineScreen />
+            </Offline>
           </DefaultLayout>
         </BrowserRouter>
       </ThemeProvider>
---
+++ src/components/common/OfflineScreen.jsx
@@ -0,0 +1,21 @@
+import WifiOffOutlinedIcon from '@mui/icons-material/WifiOffOutlined';
+import { Box, Typography } from '@mui/material';
+
+const OfflineScreen = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        pt: 10,
+        color: 'text.secondary',
+      }}
+    >
+      <WifiOffOutlinedIcon sx={{ fontSize: 140, mb: 2 }} />
+      <Typography variant="h5">No internet connection</Typography>
+    </Box>
+  );
+};
+
+export default OfflineScreen;
